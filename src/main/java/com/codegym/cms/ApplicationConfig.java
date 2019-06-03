8261592bda90b2b88108be9189c74f311a735801
HungMPhung <minhhhunggtvt2011@gmail.com>
2019-06-03 09:03:54 +0700

    Fisrt Commit Customer

--- src/main/java/com/codegym/cms/ApplicationConfig.java
+++ src/main/java/com/codegym/cms/ApplicationConfig.java
@@ -1,5 +1,7 @@
 package com.codegym.cms;
 
+import com.codegym.cms.service.CustomerService;
+import com.codegym.cms.service.impl.CustomerServiceImpl;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
@@ -7,6 +9,7 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -31,6 +34,7 @@ import java.util.Properties;
 @EnableWebMvc
 @EnableTransactionManagement
 @ComponentScan("com.codegym.cms")
+@EnableJpaRepositories("com.codegym.cms.repository")
 public class ApplicationConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
     private ApplicationContext applicationContext;
 
@@ -39,6 +43,10 @@ public class ApplicationConfig extends WebMvcConfigurerAdapter implements Applic
         this.applicationContext = applicationContext;
     }
 
+    @Bean
+    public CustomerService customerService(){
+        return new CustomerServiceImpl();
+    }
     //    Thymeleaf Configuration
     @Bean
     public SpringResourceTemplateResolver templateResolver(){
