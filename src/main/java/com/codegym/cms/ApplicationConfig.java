4a9c01819c5d05adebe44b47006c24df114e14ba
HungMPhung <minhhhunggtvt2011@gmail.com>
2019-06-03 15:45:32 +0700

    Second Commit ApplicationConfig

--- src/main/java/com/codegym/cms/ApplicationConfig.java
+++ src/main/java/com/codegym/cms/ApplicationConfig.java
@@ -1,7 +1,10 @@
 package com.codegym.cms;
 
+import com.codegym.cms.formatter.ProvinceFormatter;
 import com.codegym.cms.service.CustomerService;
+import com.codegym.cms.service.ProvinceService;
 import com.codegym.cms.service.impl.CustomerServiceImpl;
+import com.codegym.cms.service.impl.ProvinceServiceImpl;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
@@ -10,6 +13,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -43,10 +47,18 @@ public class ApplicationConfig extends WebMvcConfigurerAdapter implements Applic
         this.applicationContext = applicationContext;
     }
 
+    @Override
+    public  void addFormatters(FormatterRegistry formatterRegistry){
+        formatterRegistry.addFormatter(new ProvinceFormatter(applicationContext.getBean(ProvinceService.class)));
+    }
+
     @Bean
     public CustomerService customerService(){
         return new CustomerServiceImpl();
     }
+
+    @Bean
+    public ProvinceService provinceService(){ return new ProvinceServiceImpl(); }
     //    Thymeleaf Configuration
     @Bean
     public SpringResourceTemplateResolver templateResolver(){
@@ -114,4 +126,6 @@ public class ApplicationConfig extends WebMvcConfigurerAdapter implements Applic
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
         return properties;
     }
+
+
 }
